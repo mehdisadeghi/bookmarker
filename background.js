41deaecbfad85f6040fee16139b615f0028cf094
Mehdi Sadeghi <mehdi@mehdix.org>
2016-06-18 11:02:14 +0200

    Highlight matches.

--- background.js
+++ background.js
@@ -5,22 +5,44 @@
 // as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
+    // Prepare description text
+    var description;
+    text = text.trim();
+    text = text.replace(/,/g, ' ');
+    var inputTokens = text.split(' ');
+    inputTokens = inputTokens.filter(function(value){
+      return value != "";
+    });
     // Search bookmarks for the given text
     chrome.bookmarks.search(text, function (results) {
-		if (results) {
-			var newArray = results.map(function (currentValue, index, array) {
-				return { content: currentValue.url,
-						 description: currentValue.title.replace(text,
-						 	"<match>" + text + "</match>")};
-			});
-			try {
-				suggest(newArray);
-			}
-			catch (error) {
-				console.log(error);
-			}
-		}
-    });
+      if (results) {
+        var newArray = results.map(function (currentValue, index, array) {
+          description = currentValue.title;
+          inputTokens.forEach(function (token, index, array) {
+            var re = new RegExp(token, "ig");
+            description = description.replace(re, "<match>$&</match>");
+          });
+          return { content: currentValue.url,
+                   description: description };
+          });
+      
+      // Check results and set default message
+      var message;
+      if (newArray.length == 0) {
+        message = "Nothing interesting yet."
+      } else {
+        message = newArray.length + " results found.";
+      }
+      chrome.omnibox.setDefaultSuggestion({ "description": message });
+      
+      try {
+        suggest(newArray);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  });
 });
 
 // This event is fired with the user accepts the input in the omnibox.
